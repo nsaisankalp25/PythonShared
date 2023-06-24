ef3a43babd54eb9b246a434d55ecca5177136796
nsaisankalp25 <81605337+nsaisankalp25@users.noreply.github.com>
2023-06-24 17:46:56 +0530

    To Do List

---
+++ todolistOOPS.py
@@ -0,0 +1,45 @@
+class FileManager():
+    def __init__(self):
+        pass
+
+    def WriteToFile(self, info):
+        self.info = info
+        with open(r"C:\Users\saisa\Desktop\Coding\Course\Students\Commons\todolist.txt", "a") as file:
+            file.write(self.info + "\n")
+            
+    def ReadFile(self):
+        with open(r"C:\Users\saisa\Desktop\Coding\Course\Students\Commons\todolist.txt", "r") as file:
+            self.items = file.read().strip()
+        return self.items
+    
+class UserTalk(FileManager):
+    def __init__(self):
+        super().__init__()
+        print("Welcome to the To-Do List Application done with OOPS Python")
+        print("Enter 1 - Add a Task \n Enter 2 - View Tasks")
+        self.ask = input("")
+        if self.ask.strip() == '1':
+            self.AddTask()
+        
+        elif self.ask.strip() == '2':
+            self.ViewTasks()
+        
+        else:
+            print("Only Enter 1 or 2")
+
+    def AddTask(self):
+        print("Enter the Task in Brief")
+        self.task = input("")
+        self.task = self.task.strip()
+        self.WriteToFile(self.task)
+        print("Task Added!")
+    
+    def ViewTasks(self):
+        print("Here are your Tasks!")
+        print("---")
+        print(self.ReadFile())
+        print("---")
+
+obj = UserTalk()
+
+    
