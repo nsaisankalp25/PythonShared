f67638414721442962bb2370373955c2a2ccea8b
nsaisankalp25 <81605337+nsaisankalp25@users.noreply.github.com>
2023-08-16 21:11:00 +0530

    Add files via upload

---
+++ rev.py
@@ -0,0 +1,121 @@
+# Data Types
+# Data Structures
+# Conditions
+# Operators
+# For & While Loop
+# Inputs
+# Functions
+
+# a = 10 
+# b = 20
+
+# if a == b:
+#     print("A == B")
+# elif a > b:
+#     print(" A > B ")
+# elif a < b:
+#     print(" A < B")
+
+# else: 
+#     print("NONE OF THE ABOVE")
+
+# l = [1,2,3,4,5,6,7,8,9,10]
+# x = 5 
+
+# if x in l:
+#     print(True)
+
+# Conditions eg.
+# a == b 
+# a != b 
+# a > b 
+# a < b 
+# a <= b 
+# a >= b 
+# a in b 
+# a not in b 
+
+
+# OPERATORS
+# Arithmetic Operators - Math 
+# +
+# -
+# *
+# /
+# //    
+# **
+# %
+
+# # Comparisions
+# >
+# <
+# ==
+# != 
+# >=
+# <=
+
+# # Assignment
+# = 
+# += 
+# -=
+# *=
+# /=
+# **=
+# //=
+# %=
+
+
+# INPUTS - Data given by users to computers
+#name = input("What is your name? ")
+#print(name.upper())
+
+
+# LOOPS
+# For Loop
+# While Loop
+
+# for VARIABLE in ITERABLE:
+#     ... 
+# WE NEED A DATA SOURCE
+#for i in range(1,11):
+#    print(i)
+
+
+# while CONDITION:
+#     ...
+
+# a = 1
+# while a < 11:
+#     print(a)
+#     a += 1 # a = a + 1
+
+
+# Mutable - to change
+# Immutable - cant be changed
+
+#a = ["A", "B", "C", "D", "E"]
+#a.append("F")
+#print( a )
+
+# LIST TUPLE STRINGS
+
+# info = {"Name": "Ayan", "Age": 8, "Class": 5}
+# info2 = {"Name": "Maheen", "Age": 11, "Class": 7}
+
+# info2["Sportsperson"] = "FOOTBALL"
+# print( info2 )
+
+
+# # DATA STRUCTURES:
+# [] List # Mutable     item, item, item
+# () Tuple # Immutable  item, item, item
+# {} Dictionary # Mutable key:value, Key:value, key:value
+
+
+# # DATA TYPES:
+# bool True False 
+# float 0.24 -1340.281
+# int 681 -648
+# str "I AM THE BEST CODER!"
+
+
