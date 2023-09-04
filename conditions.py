31588389a40a713aeaff1010e0d98bf5c0ec1d52
nsaisankalp25 <81605337+nsaisankalp25@users.noreply.github.com>
2023-09-04 21:01:35 +0530

    Conditions and Operators

---
+++ conditions.py
@@ -0,0 +1,64 @@
+a = 20
+b = 20
+# starting a chain of conditions - IF
+if a == b:
+    print("A==B")
+if a > b:
+    print("A>B")
+else:
+    print("A<B")
+
+#if a == b:   # TRUE
+#    print("A==B")
+
+# 
+# Inequalities > < 
+# = -> Assignment
+# == Comparision
+
+
+# OPERATORS
+
+# Assignment
+= 
++=  
+-=
+*=
+/=
+**=
+//= # Floor division
+
+# Arithmetic
+
++
+-
+*
+/
+//
+**
+
+# Comparision
+== 
+>
+<
+>= 
+<=
+!= 
+# Logical
+
+# AND 
+True and True -> True
+False and False -> False 
+False and True -> False 
+True and False -> False
+
+# OR 
+True or True -> True
+False or False -> False 
+True or False -> True  
+False or True -> True
+ 
+
+
+
+
