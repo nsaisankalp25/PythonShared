3496a1489234db85afccdade1fc872097525f7f7
SSankalp N <81605337+nsaisankalp25@users.noreply.github.com>
2024-12-06 20:14:47 +0530

    Update class 2.py

--- class 2.py
+++ class 2.py
@@ -2,27 +2,27 @@
 
 
 
-# age = input("What is your age? ")
+age = input("What is your age? ")
 
-# print(age*5)
+print(age*5)
 
 #int/float/bool -> str 
 
-# a = 10
-# b = 2.5
-# d = True
+a = 10
+b = 2.5
+d = True
 
-# c1 = str(a)
-# c2 = str(b)
-# c3 = str(d)
+c1 = str(a)
+c2 = str(b)
+c3 = str(d)
 
-# print(c1)
-# print(c2)
-# print(c3)
+print(c1)
+print(c2)
+print(c3)
 
-# print(type(c1))
-# print(type(c2))
-# print(type(c3))
+print(type(c1))
+print(type(c2))
+print(type(c3))
 
 
 #str -> int/float
@@ -33,67 +33,67 @@ c = "Hello 2024" # NOT WORKING
 d = '22 & 23' # NOT WORKING
 e = "-6.7"
 
-#a2 = int(a)
-#print(a2)
+a2 = int(a)
+print(a2)
 
-# b2 = int(b)
-# print(b2)
+b2 = int(b)
+print(b2)
 
-# c2 = int(c)
-# print(c2)
+c2 = int(c)
+print(c2)
 
-# d2 = int(d)
-# print(d2)
+d2 = int(d)
+print(d2)
 
-# e2 = float(e)
-# print(e2)
+e2 = float(e)
+print(e2)
 
-# e3 = int(e2)
-# print(e3)
+e3 = int(e2)
+print(e3)
 
 
 
 # str -> bool 
 
-# a = "HELLO" 
-# b = "1234" 
-# c = "Hello 2024"
-# d = ' ' 
-# e = ""
+a = "HELLO" 
+b = "1234" 
+c = "Hello 2024"
+d = ' ' 
+e = ""
 
-# a2 = bool(a)
-# print(a2)
+a2 = bool(a)
+print(a2)
 
-# b2 = bool(b)
-# print(b2)
+b2 = bool(b)
+print(b2)
 
-# c2 = bool(c)
-# print(c2)
+c2 = bool(c)
+print(c2)
 
-# d2 = bool(d)
-# print(d2)
+d2 = bool(d)
+print(d2)
 
-# e2 = bool(e)
-# print(e2)
+e2 = bool(e)
+print(e2)
 
 
 # BOOL -> int/float
 
-# a = True
-# b = False
+a = True
+b = False
 
 
-# a2 = int(a)
-# print(a2)
+a2 = int(a)
+print(a2)
 
-# b2 = int(b)
-# print(b2)
+b2 = int(b)
+print(b2)
 
-# a3 = float(a)
-# print(a3)
+a3 = float(a)
+print(a3)
 
-# b3 = float(b)
-# print(b3)
+b3 = float(b)
+print(b3)
 
 
 # INT/float -> bool
