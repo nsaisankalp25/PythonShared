1c6d7cd6bea1a4d037f75761e128987ab13b5898
SSankalp N <81605337+nsaisankalp25@users.noreply.github.com>
2024-12-06 20:11:22 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,112 @@
+# Ask the name of the user
+
+
+
+# age = input("What is your age? ")
+
+# print(age*5)
+
+#int/float/bool -> str 
+
+# a = 10
+# b = 2.5
+# d = True
+
+# c1 = str(a)
+# c2 = str(b)
+# c3 = str(d)
+
+# print(c1)
+# print(c2)
+# print(c3)
+
+# print(type(c1))
+# print(type(c2))
+# print(type(c3))
+
+
+#str -> int/float
+
+a = "HELLO" # NOT WORKING
+b = "1234" # WORKING
+c = "Hello 2024" # NOT WORKING
+d = '22 & 23' # NOT WORKING
+e = "-6.7"
+
+#a2 = int(a)
+#print(a2)
+
+# b2 = int(b)
+# print(b2)
+
+# c2 = int(c)
+# print(c2)
+
+# d2 = int(d)
+# print(d2)
+
+# e2 = float(e)
+# print(e2)
+
+# e3 = int(e2)
+# print(e3)
+
+
+
+# str -> bool 
+
+# a = "HELLO" 
+# b = "1234" 
+# c = "Hello 2024"
+# d = ' ' 
+# e = ""
+
+# a2 = bool(a)
+# print(a2)
+
+# b2 = bool(b)
+# print(b2)
+
+# c2 = bool(c)
+# print(c2)
+
+# d2 = bool(d)
+# print(d2)
+
+# e2 = bool(e)
+# print(e2)
+
+
+# BOOL -> int/float
+
+# a = True
+# b = False
+
+
+# a2 = int(a)
+# print(a2)
+
+# b2 = int(b)
+# print(b2)
+
+# a3 = float(a)
+# print(a3)
+
+# b3 = float(b)
+# print(b3)
+
+
+# INT/float -> bool
+
+# all numbers are true, except 0
+
+a = 0
+b = 253.43
+
+
+a2 = bool(a)
+print(a2)
+
+b2 = bool(b)
+print(b2)
+
