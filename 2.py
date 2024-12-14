63d450e672608882d4bdd30cf1a73b0ac9f238a4
SSankalp N <81605337+nsaisankalp25@users.noreply.github.com>
2024-12-14 23:08:10 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,132 @@
+a = "Hello"
+b = True
+c = 5 # int -> non decimal number
+d = 15.6 # float -> decimal number
+
+# age = input("Enter your age: ")
+# print(type(age))
+
+# age2 = int(age)
+
+# print(type(age2))
+
+# print(6*age2)
+
+
+
+# DATA CONVERSIONS
+
+# int -> float
+
+x = 10
+a = float(x)
+# print(a)
+
+# float -> int
+y =  6.999
+s = int(y)
+
+# print(s)
+
+# int/float -> str
+p = 5
+q = 8.65
+
+z1 = str(p)
+z2 = str(q)
+
+
+# print(z1)
+# print(z2)
+
+# print(z1 + z2)
+# str + str -> string concatenation
+
+# print(z1 * 3)
+# str * int -> multiple strings
+
+
+
+# bool -> int/float
+
+h = True # 1
+g = False # 0
+
+h1 = int(h)
+h2 = float(h)
+
+g1 = int(g)
+g2 = float(g)
+
+# print(h1)
+# print(g1)
+
+
+
+# int/float/str -> bool
+
+x = 0
+y = 24.5
+z1 = "HELLO" # true
+z2 = "" # false
+
+# true for all values, except 0
+
+x1 = bool(x)
+y1 = bool(y)
+
+z3 = bool(z1)
+z4 = bool(z2)
+
+# print(z3)
+# print(z4)
+
+
+# str -> int/float/bool
+
+s1 = "HELLO"
+s2 = "123341" # can be into int
+s3 = "-235.523523" # can be into float
+s4 = "Time is 12 am"
+s5 = "22+45"
+
+#b1 = int(s1)
+# b2 = int(s2)
+# print(b2)
+
+# b3 = float(s3)
+# print(b3)
+
+# -----------
+
+# python -> 2 numbers
+# c, c++, Java -> 12 
+
+# infinity
+
+# float >>>>> int
+
+
+# * -> multiplication
+# ** -> exponents
+
+# j = 3*(10**10000064684889494984994984)
+# print(j)
+
+#print(float("-inf"))
+
+#print(7**0.1)
+
+
+# ROUNDING NUMBERS
+
+
+
+a = 7**0.01
+b = 23235823823.5235235
+
+print(a)
+print( round(b, -5) ) # round off whole number part
+
+
+print( round(a, 5) )  # round off decimals
